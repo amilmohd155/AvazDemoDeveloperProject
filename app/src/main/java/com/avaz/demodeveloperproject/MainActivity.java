0f42eb09c4f4809c8337f8db036895ad4d5d8dd9
amilmohd155 <amilmohd155@gmail.com>
2021-12-08 12:56:24 +0530

    Changed to single activity method

--- app/src/main/java/com/avaz/demodeveloperproject/MainActivity.java
+++ app/src/main/java/com/avaz/demodeveloperproject/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.avaz.demodeveloperproject.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
     }
 }
---
+++ app/src/main/java/com/avaz/demodeveloperproject/fragment/MainFragment.java
@@ -0,0 +1,37 @@
+package com.avaz.demodeveloperproject.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.avaz.demodeveloperproject.R;
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+
+    }
+
+    public static MainFragment newInstance() {
+        MainFragment fragment = new MainFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
