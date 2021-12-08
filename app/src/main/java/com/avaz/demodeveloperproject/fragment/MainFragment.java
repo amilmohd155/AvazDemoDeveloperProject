2b7d613f565c76f22dd5469f4a77ca90047fc675
amilmohd155 <amilmohd155@gmail.com>
2021-12-08 13:28:39 +0530

    Recycler View Adapter in development

--- app/src/main/java/com/avaz/demodeveloperproject/fragment/MainFragment.java
+++ app/src/main/java/com/avaz/demodeveloperproject/fragment/MainFragment.java
@@ -2,23 +2,30 @@ package com.avaz.demodeveloperproject.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.avaz.demodeveloperproject.R;
+import com.avaz.demodeveloperproject.databinding.FragmentMainBinding;
 
 public class MainFragment extends Fragment {
+
+    private FragmentMainBinding binding;
+    private RecyclerView rvDefault, rvDynamic;
 
     public MainFragment() {
 
     }
 
     public static MainFragment newInstance() {
-        MainFragment fragment = new MainFragment();
-        return fragment;
+        return new MainFragment();
     }
 
     @Override
@@ -30,8 +37,23 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
-        return view;
+
+        binding = FragmentMainBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        rvDefault = binding.rvDefaultList;
+
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
---
+++ app/src/main/java/com/avaz/demodeveloperproject/utility/CardAdapter.java
@@ -0,0 +1,32 @@
+package com.avaz.demodeveloperproject.utility;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CardAdapter extends RecyclerView.Adapter<CardAdapter.Viewholder> {
+
+    @NonNull
+    @Override
+    public Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Viewholder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class Viewholder extends RecyclerView.ViewHolder{
+        public Viewholder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
